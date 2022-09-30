53d8377031fcc359cafa67b3c700c341c5bfa72d
TheLastSunset <88444633+TheLastSunset@users.noreply.github.com>
2022-09-30 21:27:08 +0800

    配置优化

--- MynahMoreInfo/AfterBuild.ps1
+++ MynahMoreInfo/AfterBuild.ps1
@@ -1,8 +1,13 @@
 Set-Location $PSScriptRoot
 
+$ModName = "MynahMoreInfo"
+
+New-Item "$env:TAIWU_PATH\Mod\$ModName" -ItemType "directory"
+New-Item "$env:TAIWU_PATH\Mod\$ModName\Plugins" -ItemType "directory"
+
 & "../../../MynahModConfigGenerator/bin/Debug/net6.0/MynahModConfigGenerator.exe" Config.lua MynahMoreInfo.dll
 
-Copy-Item *.dll "C:\SteamLibrary\steamapps\common\The Scroll Of Taiwu\Mod\MynahMoreInfo\Plugins" -Verbose
-Copy-Item Config.lua "C:\SteamLibrary\steamapps\common\The Scroll Of Taiwu\Mod\MynahMoreInfo" -Verbose
+Copy-Item *.dll "$env:TAIWU_PATH\Mod\$ModName\Plugins" -Verbose
+Copy-Item Config.lua "$env:TAIWU_PATH\Mod\$ModName" -Verbose
 
 Write-Output "copy over"
