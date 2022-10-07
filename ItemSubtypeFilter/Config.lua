3b47088f615f659ed1a7cbb5212db806d35dea0b
12548 <gsc12548@gmail.com>
2022-10-07 14:47:37 +0800

    补充已发布mod的创意工坊Config

--- ItemSubtypeFilter/Config.lua
+++ ItemSubtypeFilter/Config.lua
@@ -2,6 +2,9 @@ return {
     Title = "~物品高级筛选 v0.6~",
     FrontendPlugins = {"MynahBaseModBase.dll", "MynahBaseModFrontend.dll", "ItemSubtypeFilter.dll"},
     Author = "myna12548",
+    FileId = 2871616225,
+    Source = 1,
+    Cover = "cover.png",
     Description = "对物品增加二级筛选、书籍三级筛选。可用于：行囊、仓库、商店、事件（偷窃等）、书籍交换、茶马帮等界面。\n（首次打开界面时无效，切换一下一级筛选即可出现二级筛选）",
     DefaultSettings = {
     }
--- MynahMoreInfo/Config.lua
+++ MynahMoreInfo/Config.lua
@@ -2,6 +2,9 @@ return {
     Title = "~更多信息 v1.0~",
     FrontendPlugins = {"MynahBaseModBase.dll", "MynahBaseModFrontend.dll", "MynahMoreInfo.dll"},
     Author = "myna12548",
+    Source = 1,
+    FileId = 2871621277,
+    Cover = "cover.png",
     Description = "人物增加浮窗，显示功法正逆练效果、打点、学习进度和不传之秘，显示书籍所载功法正逆练(如果遇到书籍的正逆练不显示的情况，请先把鼠标放到一个功法上显示一下)",
     DefaultSettings = {
     }
