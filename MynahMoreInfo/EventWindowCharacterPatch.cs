7918670d8c4aab5aae5f44576a36c45afcb0d47e
12548 <gsc12548@gmail.com>
2023-11-21 19:45:06 +0800

    更多信息2.3

---
+++ MynahMoreInfo/EventWindowCharacterPatch.cs
@@ -0,0 +1,38 @@
+﻿using GameData.Domains.CombatSkill;
+using HarmonyLib;
+using TMPro;
+using UnityEngine;
+
+namespace MynahMoreInfo;
+
+[HarmonyPatch]
+public class EventWindowCharacterPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(EventWindowCharacter), "Refresh")]
+    public static void Postfix(EventWindowCharacter __instance)
+    {
+        Debug.Log("EventWindowCharacterPatch1");
+        if (!ModEntry.ShowEventUICharacterMouseTip) return;
+        Debug.Log("EventWindowCharacterPatch2");
+        if (__instance != null)
+        {
+            Debug.Log("EventWindowCharacterPatch3");
+            var mou = Util.EnsureMouseTipDisplayer(__instance.transform.Find("CanvasChanger/AvatarArea/ShowCharacterMenu").gameObject);
+            mou.enabled = __instance.GetHasCharacter();
+            if (mou.enabled)
+            {
+                Util.EnableMouseTipCharacter(mou,
+                    __instance.IsLeftCharacter
+                        ? __instance.Data.MainCharacter.CharacterId
+                        : __instance.Data.TargetCharacter.CharacterId,
+                    ModEntry.ReplaceAllCharacterTipToDetail ? 2 : 1);
+                Debug.Log("EventWindowCharacterPatch4");
+            }
+
+            Debug.Log("EventWindowCharacterPatch5");
+        }
+
+        
+    }
+}
